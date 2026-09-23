ad39c12db784c89db56496f53de31053d6a7cf11
souravbrock <souravbrock@gmail.com>
2026-09-24 02:20:36 +0530

    CORS: allow X-GBPS-Token header (unblocks APK login)

--- backend/api.php
+++ backend/api.php
@@ -33,7 +33,7 @@ function cors(): void {
     header('Access-Control-Allow-Origin: ' . ($origin !== '' ? $origin : '*'));
     header('Vary: Origin');
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-    header('Access-Control-Allow-Headers: Content-Type, Authorization');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization, X-GBPS-Token');
     if (($_SERVER['REQUEST_METHOD'] ?? 'GET') === 'OPTIONS') {
         http_response_code(204);
         exit;
